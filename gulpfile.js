14e6f75a2ff4a4cefc3b02ea85fdc85be6a6bfa0
ValeriaMaltseva <lerok.maltseva96@mail.ru>
2016-07-25 15:28:12 +0300

    Setting up environment

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp'),
+    rename = require("gulp-rename"),
+    minifyCSS = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function () {
+    return gulp.src('./src/css/**/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 22 versions', 'ie 8', 'ie 9'],
+            cascade: false
+        }))
+        .pipe(minifyCSS())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('image', function () {
+    return gulp.src('./src/image/**/*')
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', ['css', 'image'], function () {
+    gulp.watch('./src/css/**/*.css', ['css']);
+    gulp.watch('./src/image/**/*', ['image']);
+});
+
+
+
+
+
+
+
+
